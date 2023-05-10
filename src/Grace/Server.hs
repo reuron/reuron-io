6df7b434d1603a355d28dcdb0414d7f5be83883a
Greg Hale <imalsogreg@gmail.com>
2023-05-10 16:55:46 -0700

    Add swc simplification to convert-swc endpoint

--- src/Grace/Server.hs
+++ src/Grace/Server.hs
@@ -1,12 +1,21 @@
 -- |
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Grace.Server where
 
 import qualified Control.Monad.Except as Except
 import qualified Data.Text.Encoding as Text
+import qualified Data.List as List
+import Data.Maybe (mapMaybe)
+import qualified Data.Map as Map
+import Data.Map (Map)
+import qualified Data.Set as Set
+import Data.Set (Set)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as LBS
+import Data.Scientific (Scientific)
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Middleware.Cors as Cors
 import qualified Network.Wai as Wai
@@ -20,6 +29,7 @@ import qualified Grace.Interpret as Interpret
 import Grace.Input (Input(Code))
 import qualified Grace.Normalize as Normalize
 import qualified Grace.Pretty as Pretty
+
 
 -- TODO: Harden (maxinum body length, encoding)
 serve :: Int -> IO ()
@@ -36,6 +46,10 @@ serve port = Warp.run port $ Cors.simpleCors $ \req respond ->
           respond $ Wai.responseLBS HTTP.status200 [] (LBS.fromStrict $ Text.encodeUtf8 response)
     ["convert-swc"] -> do
       swcBytes <- Wai.lazyRequestBody req
+      let simplifyDegree = case List.lookup "degree" (Wai.queryString req) of
+            Just (Just bs) -> Just $ read @Int (BS.unpack bs)
+            Just Nothing -> error "Invalid degree parameter"
+            Nothing -> Nothing
       swcContents <- case HTTP.parseRequest (LBS.unpack swcBytes) of
             Nothing -> return swcBytes
             Just url -> do
@@ -43,7 +57,13 @@ serve port = Warp.run port $ Cors.simpleCors $ \req respond ->
               resp <- HTTP.httpLbs url mgr
               let body = HTTP.responseBody resp
               return $ body
-      let ffgBytes = swcContentsToFfg swcContents
+      let
+        swcLines = parseSwcLines swcContents
+        simplifiedSwcLines = case simplifyDegree of
+          Nothing -> swcLines
+          Just n -> simplifySwc n swcLines
+        ffgBytes = encodeSwcLines simplifiedSwcLines
+
       respond $ Wai.responseBuilder HTTP.status200 [] ffgBytes
     ["echo"] -> do
       bytes <- Wai.lazyRequestBody req
@@ -51,11 +71,29 @@ serve port = Warp.run port $ Cors.simpleCors $ \req respond ->
     _ -> Wai.staticApp (Wai.defaultWebAppSettings "static") req respond
     -- _ -> respond $ Wai.responseLBS HTTP.status404 [] "Not found"
 
+parseSwcLines :: LBS.ByteString -> [SwcLine]
+parseSwcLines swcBytes = mapMaybe parseLine (LBS.lines swcBytes)
+  where
+    parseLine :: LBS.ByteString -> Maybe SwcLine
+    parseLine line =
+      let trim str = LBS.filter (/= ' ') str
+      in
+      case LBS.words line of
+        [nId', nType', x', y', z', radius', parent'] -> Just $ SwcLine
+          { id_ = read (LBS.unpack $ trim nId')
+          , x   = read (LBS.unpack $ trim x')
+          , y   = read (LBS.unpack $ trim y')
+          , z   = read (LBS.unpack $ trim z')
+          , r   = read (LBS.unpack $ trim radius')
+          , parent_ = read (LBS.unpack $ trim parent')
+          , type_ = read (LBS.unpack $ trim nType')
+          }
+        _ -> Nothing -- error "bad line"
 
-swcContentsToFfg :: LBS.ByteString -> Builder.Builder
-swcContentsToFfg input =
+encodeSwcLines :: [SwcLine] -> Builder.Builder
+encodeSwcLines swcLines =
   header
-  <> (mconcat (swcLine <$> LBS.lines input))
+  <> (mconcat (swcLine <$> swcLines))
   <> footer
   where
     header :: Builder.Builder
@@ -65,25 +103,78 @@ swcContentsToFfg input =
       \in \n defaultSwcNeuron [ \
       \"
 
-    swcLine :: LBS.ByteString -> Builder.Builder
-    swcLine line =
-      let trim str = LBS.filter (/= ' ') str
-      in
-      case LBS.words line of
-        [nId, nType, x, y, z, radius, parent] ->
-          Builder.byteString . BS.pack . concat $
-          ["  {id: ",    LBS.unpack (trim nId)
-          ,", x: ",      LBS.unpack (trim x)
-          ,", y: ",      LBS.unpack (trim y)
-          ,", z: ",      LBS.unpack (trim z)
-          ,", r: ",      LBS.unpack (trim radius)
-          ,", type: ",   LBS.unpack (trim nType)
-          ,", parent: ", LBS.unpack (trim parent)
-          ,"},\n"
-          ]
-        _ -> mempty
-
+    swcLine :: SwcLine  -> Builder.Builder
+    swcLine SwcLine { id_, x, y, z, r, type_, parent_ } =
+        Builder.byteString . BS.pack . concat $
+        ["  {id: ",    show id_
+        ,", x: ",      show x
+        ,", y: ",      show y
+        ,", z: ",      show z
+        ,", r: ",      show r
+        ,", type: ",   show type_
+        ,", parent: ", show parent_
+        ,"},\n"
+        ]
 
 
     footer :: Builder.Builder
     footer = "\n]"
+
+
+data SwcLine = SwcLine
+  { id_ :: Int
+  , x :: Scientific
+  , y :: Scientific
+  , z :: Scientific
+  , r :: Scientific
+  , type_ :: Int
+  , parent_ :: Int
+  }
+
+simplifySwc :: Int -> [SwcLine] -> [SwcLine]
+simplifySwc degree swcLines =
+  let
+    children :: Map Int [Int]
+    children =
+      List.foldl'
+      (\acc SwcLine{id_, parent_} -> Map.insertWith (++) parent_ [id_] acc)
+      mempty
+      swcLines
+
+    entities :: Map Int SwcLine
+    entities = Map.fromList [(id_, x) | x@SwcLine{id_} <- swcLines]
+
+    keepableLines :: [SwcLine]
+    keepableLines = List.filter keep swcLines
+      where
+        keep SwcLine {id_} =
+          let
+            is_first = id_ == 1
+            is_branch_or_leaf =
+              case Map.lookup id_ children of
+                Nothing -> True
+                Just n -> List.length n > 1
+            is_downsample = id_ `mod` degree == 0
+          in is_first
+             || is_branch_or_leaf
+             || is_downsample
+
+    shouldKeep :: Set Int
+    shouldKeep = Set.fromList $ map id_ $ keepableLines
+
+    seekKeepableEntity :: Int -> Int
+    seekKeepableEntity eid = case Map.lookup eid entities of
+      Just SwcLine { parent_ } ->
+        if eid `Set.member` shouldKeep
+        then eid
+        else seekKeepableEntity parent_
+      Nothing -> error $ "Unknown entity " ++ show eid
+
+    adjustParent :: SwcLine -> SwcLine
+    adjustParent s@SwcLine {parent_} =
+      if parent_ == -1
+      then s -- Entities with no parent don't get parent adjustment.
+      else s { parent_ = seekKeepableEntity parent_ }
+
+  in
+    map adjustParent $ keepableLines
